359fafba1b2e901a603626e49f76457acbe3650f
Stefan Jaroschek <stefan@jaroschek.org>
2025-01-08 14:43:09 +0100

    Allow parallel entity updates as a central coordinator is used

--- custom_components/eaton_epdu/sensor.py
+++ custom_components/eaton_epdu/sensor.py
@@ -36,7 +36,7 @@ from .const import (
 from .coordinator import SnmpCoordinator
 from .entity import SnmpEntity
 
-PARALLEL_UPDATES = 1
+PARALLEL_UPDATES = 0
 SCAN_INTERVAL = timedelta(seconds=60)
 
 
